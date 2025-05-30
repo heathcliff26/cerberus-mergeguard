bf4c064781e4ba9f1f717afc835cb7d15b8e20ca
Heathcliff <heathcliff@heathcliff.eu>
2025-05-30 14:19:55 +0200

    Implemented webhook handler
    Add handler for pull_request and check_run events.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,18 @@
-use crate::client::Client;
-use axum::{Json, Router, http::StatusCode, routing::get};
+use crate::{client::Client, types::CheckRunEvent, types::PullRequestEvent};
+use axum::{
+    Json, Router,
+    extract::State,
+    http::{HeaderMap, HeaderValue, StatusCode},
+    routing::{get, post},
+};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use tokio::signal;
 use tower_http::trace::TraceLayer;
-use tracing::info;
+use tracing::{debug, error, info, warn};
 
 pub const SERVER_STATUS_OK: &str = "ok";
+pub const SERVER_STATUS_ERROR: &str = "error";
 pub const SERVER_MESSAGE_OK: &str = "Server is running fine";
 
 /// Options for the http server
@@ -108,6 +114,7 @@ impl Server {
 
         let router: Router = Router::new()
             .route("/healthz", get(healthz))
+            .route("/webhook", post(webhook_handler))
             .with_state(state)
             .layer(TraceLayer::new_for_http());
 
@@ -127,26 +134,203 @@ impl Server {
 
 /// Expose health check endpoint
 /// Can be used when running under kubernetes to check if the server is running
-async fn healthz() -> (StatusCode, Json<HealthCheckResponse>) {
-    (StatusCode::OK, Json(HealthCheckResponse::new()))
+/// GET /healthz
+async fn healthz() -> (StatusCode, Json<Response>) {
+    (StatusCode::OK, Json(Response::new()))
+}
+
+/// Handle the webhook events send from GitHub
+/// POST /webhook
+async fn webhook_handler(
+    headers: HeaderMap,
+    state: State<ServerState>,
+    payload: String,
+) -> (StatusCode, Json<Response>) {
+    let event = match headers.get("X-GitHub-Event") {
+        Some(event) => event
+            .to_str()
+            .unwrap_or("could not read X-GitHub-Event header"),
+        None => {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(Response::error("Missing X-GitHub-Event header")),
+            );
+        }
+    };
+    debug!("Received webhook event: {}", event);
+    if let Err(e) = verify_webhook(
+        headers.get("X-Hub-Signature-256"),
+        state.webhook_secret.as_deref(),
+        &payload,
+    ) {
+        warn!("Failed to verify webhook signature: {}", e.1.message);
+        return e;
+    }
+
+    match event {
+        "check_run" => handle_check_run_event(&state.github, &payload).await,
+        "pull_request" => handle_pull_request_event(&state.github, &payload).await,
+        event => {
+            let message = format!("Received unsupported event: {}", event);
+            info!("{message}");
+            (StatusCode::NOT_IMPLEMENTED, Json(Response::error(&message)))
+        }
+    }
+}
+
+/// Verify the webhook request against the shared secret
+fn verify_webhook(
+    signature: Option<&HeaderValue>,
+    secret: Option<&str>,
+    _payload: &str,
+) -> Result<(), (StatusCode, Json<Response>)> {
+    if secret.is_none() {
+        return Ok(());
+    }
+    if signature.is_none() {
+        return Err((
+            StatusCode::FORBIDDEN,
+            Json(Response::error("Missing X-Hub-Signature-256 header")),
+        ));
+    }
+    // TODO: Implement actual signature verification logic
+    Ok(())
+}
+
+/// Handle webhook pull_request events
+async fn handle_pull_request_event(client: &Client, payload: &str) -> (StatusCode, Json<Response>) {
+    let payload: PullRequestEvent = match serde_json::from_str(payload) {
+        Ok(event) => event,
+        Err(e) => {
+            warn!("Failed to parse pull_request event payload: {e}");
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(Response::error("Invalid pull_request event payload")),
+            );
+        }
+    };
+
+    match payload.action.as_str() {
+        "opened" | "synchronize" => {}
+        action => {
+            debug!("Ignoring pull_request event with action: {action}");
+            return (StatusCode::OK, Json(Response::new()));
+        }
+    }
+
+    let app_id = match payload.installation {
+        Some(installation) => installation.id,
+        None => {
+            warn!("Missing app installation id in pull_request event");
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(Response::error("Missing app installation id")),
+            );
+        }
+    };
+
+    if let Err(e) = client
+        .create_check_run(
+            app_id,
+            &payload.repository.full_name,
+            &payload.pull_request.head.sha,
+        )
+        .await
+    {
+        error!("Failed to create check run: {e}");
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(Response::error("Failed to create check-run")),
+        );
+    };
+    info!(
+        "Created check run for pull request {}",
+        payload.pull_request.number
+    );
+    (StatusCode::OK, Json(Response::new()))
+}
+
+/// Handle webhook check_run events
+async fn handle_check_run_event(client: &Client, payload: &str) -> (StatusCode, Json<Response>) {
+    let payload: CheckRunEvent = match serde_json::from_str(payload) {
+        Ok(event) => event,
+        Err(e) => {
+            warn!("Failed to parse check_run event payload: {e}");
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(Response::error("Invalid check_run event payload")),
+            );
+        }
+    };
+
+    let app_id = match payload.installation {
+        Some(installation) => installation.id,
+        None => {
+            warn!("Missing app installation id in check_run event");
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(Response::error("Missing app installation id")),
+            );
+        }
+    };
+
+    let (uncompleted, own_run) = match client
+        .get_check_run_status(
+            app_id,
+            &payload.repository.full_name,
+            &payload.check_run.head_sha,
+        )
+        .await
+    {
+        Ok(check_runs) => check_runs,
+        Err(e) => {
+            error!("{e}");
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(Response::error("Failed to get check-runs")),
+            );
+        }
+    };
+    if let Err(e) = client
+        .update_check_run(
+            app_id,
+            &payload.repository.full_name,
+            &payload.check_run.head_sha,
+            uncompleted == 0,
+            own_run,
+        )
+        .await
+    {
+        error!("Failed to update check-run: {e}");
+    }
+    (StatusCode::OK, Json(Response::new()))
 }
 
 /// Detailed status of the Webserver
 #[derive(Debug, Serialize, Deserialize)]
-pub struct HealthCheckResponse {
+pub struct Response {
     /// Status of the server.
     /// "ok" if everything is running fine, "error" if something is wrong.
+    // TODO: use &str
     pub status: String,
     /// Optional message providing more details about the status.
     pub message: String,
 }
 
-impl HealthCheckResponse {
-    /// Create a new health check response with "ok" status.
+impl Response {
+    /// Create a new response with ok status.
     pub fn new() -> Self {
         Self {
             status: SERVER_STATUS_OK.to_string(),
             message: SERVER_MESSAGE_OK.to_string(),
+        }
+    }
+
+    /// Create a new response with the error status.
+    pub fn error(message: &str) -> Self {
+        Self {
+            status: SERVER_STATUS_ERROR.to_string(),
+            message: message.to_string(),
         }
     }
 }
